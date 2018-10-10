25cce668e500cbed7f6031aeeafd8ce6afa81291
Bao Tran <trangiabao1203@gmail.com>
2018-10-10 22:41:43 +0700

    Use Shared Component

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -8,13 +9,15 @@ import { ProductFormComponent } from './product-form/product-form.component';
 @NgModule({
   imports : [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    SharedModule
   ],
   declarations : [
     ProductDetailComponent,
     ProductListComponent,
     ProductFormComponent
-  ]
+  ],
+  exports : []
 })
 
 export class ProductModule {
