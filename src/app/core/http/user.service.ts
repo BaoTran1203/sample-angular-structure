f94edb29bb471686f6d0f102a8a9c3f8b413e69f
Bao Tran <trangiabao1203@gmail.com>
2018-10-11 00:50:39 +0700

    HttpClient

---
+++ src/app/core/http/user.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CONFIG } from '../../configs/config';
+import * as $ from 'jquery';
+
+@Injectable({
+  providedIn : 'root'
+})
+
+export class UserService {
+
+  private url_endPoint = `${CONFIG.apiUrl}/user`;
+
+  constructor(public http: HttpClient) {
+  }
+
+  list(params: {}): any {
+    const query = $.param(params);
+    const url = `${this.url_endPoint}?${query}`;
+    return this.http.get(url);
+  }
+
+  add(model): any {
+    const url = `${this.url_endPoint}`;
+    return this.http.post(url, model);
+  }
+
+  detail(id): any {
+    const url = `${this.url_endPoint}/${id}`;
+    return this.http.get(url);
+  }
+
+  update(model): any {
+    const url = `${this.url_endPoint}`;
+    return this.http.put(url, model);
+  }
+
+  delete(model): any {
+    const url = `${this.url_endPoint}/${model._id}`;
+    return this.http.delete(url);
+  }
+}
